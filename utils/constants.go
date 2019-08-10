363459918072c1b4b5b55a7e74e7f05d2b791be7
truonglv <truonglevan@admicro.vn>
2019-08-10 17:26:16 +0700

    add log event

--- utils/constants.go
+++ utils/constants.go
@@ -4,10 +4,10 @@ const (
 	os_code_iphone  = 7
 	os_code_android = 8
 
-	event_id_start_app   = 1
-	event_id_restart_app = 2
-	event_id_stop_app    = 3
+	event_id_start_app   = 1001
+	event_id_restart_app = 1002
+	event_id_stop_app    = 1003
 
-	event_id_click_event   = 4
-	event_id_click_article = 5
+	event_id_click_event   = 2001
+	event_id_click_article = 2002
 )
