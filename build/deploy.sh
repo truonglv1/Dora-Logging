5f3678469360d567a0bf2ddfb98fcf7eeff56ede
truonglv <truonglevan@admicro.vn>
2019-08-10 17:08:30 +0700

    add deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,3 +3,8 @@ echo "remove application..."
 rm bin/dora-logging
 echo "build application..."
 go build -o bin/dora-logging cmd/main.go
+echo "upFie..."
+rsync -avzP -r --delete \
+    configs \
+    bin/dora-logging \
+    doraemon@110.35.75.45:/home/doraemon/truonglv/Dora-Logging/
